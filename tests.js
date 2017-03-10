e0d15667ebd6a8ed56af0dbb6657c44a94770891
Nic Mitchell <nic@nicmitchell.com>
2017-03-09 17:28:40 -0800

    added sanity tests for superbalanced-binary-tree

--- tests.js
+++ tests.js
@@ -2,9 +2,20 @@ var assert = chai.assert;
 var expect = chai.expect;
 
 describe('Superbalanced binary tree', function() {
+  const tree = new BinaryTreeNode(10);
+  tree.insertLeft(5);
+  tree.insertRight(7);
 
-  it('Can create a test', function() {
-    expect(0).to.equal(0);
+  it('Can create a binary tree node with a value', function() {
+    expect(tree.value).to.equal(10);
   });
 
+  it('Can can insert left and right values', function() {  
+    expect(tree.left.value).to.equal(5); 
+    expect(tree.right.value).to.equal(7); 
+  });
+
+  it('Child nodes are instances of BinaryTreeNode', function() {
+    expect(tree.left instanceof BinaryTreeNode).to.be.true;
+  });
 });
