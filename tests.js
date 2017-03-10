0244746751781edd54ae3fde251f6d891c30dede
Nic Mitchell <nic@nicmitchell.com>
2017-03-09 17:05:38 -0800

    sanity for test suite

---
+++ tests.js
@@ -0,0 +1,10 @@
+var assert = chai.assert;
+var expect = chai.expect;
+
+describe('Superbalanced binary tree', function() {
+
+  it('Can create a test', function() {
+    expect(0).to.equal(0);
+  });
+
+});
