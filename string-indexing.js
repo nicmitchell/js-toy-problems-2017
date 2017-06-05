f7e22490202db639f2837f03b83bbbbb43233e4b
Nic Mitchell <nic@nicmitchell.com>
2017-06-04 18:37:01 -0700

    refactored trie with prototype methods

--- string-indexing.js
+++ string-indexing.js
@@ -1,40 +1,45 @@
 /*
 String indexing: Keep track of which web URLs have been visited.
+
+Use the following methods:
+insert: Inserts a word into the trie.
+search: Returns if the word is in the trie and is a complete word. 
+startsWith: Returns if there is any word in the trie that starts with the given prefix. 
  */
 
-function searchTrie(node, string) {
-  if (string === null) {
+var Trie = function(char) {
+    this.root = {};
+};
+
+Trie.prototype.insert = function(word) {
+    let current = this.root;
+    for (let char of word) {
+        if (!current[char]) {
+            current[char] = {};
+        }
+        current = current[char];
+    }
+    current.isWord = true;
+};
+
+Trie.prototype.search = function(word) {
+    let current = this.root;
+    for (let char of word) { 
+        if (!current.hasOwnProperty(char)) {
+            return false;
+        }
+        current = current[char];
+    }
+    return current.isWord === true;
+};
+
+Trie.prototype.startsWith = function(prefix) {
+    let current = this.root;
+    for (let char of prefix) {
+        if (!current.hasOwnProperty(char)) {
+            return false;
+        }
+        current = current[char];
+    }
     return true;
-  }
-
-  const char = string.charAt(0);
-  const remaining = string.slice(1) || null;
-
-  if (node[char]) {
-    return searchTrie(node[char], remaining);
-  }
-
-  return false;
-}
-
-function addURL(node, url) {
-
-  let char = url.charAt(0);
-  let remaining = url.slice(1) || null;
-
-  if (!node[char]) {
-    return insertString(node, remaining);
-  }
-}
-
-function insertString(node, string) {
-  if (string === null) {
-    return true;
-  }
-  let char = string.charAt(0);
-  let remaining = string.slice(1) || null
-  if (!node[char]) {
-    node[char] = {};
-  }
-  return insertString(node[char], remaining);
-}
+};
